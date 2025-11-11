43c81ad87ed9392a8a5f4f33108f775f1b911c01
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-11-11 21:31:59 +0500

    operators

--- Operators/script.js
+++ Operators/script.js
@@ -15,6 +15,31 @@ console.log(b--);
 console.log(--b);
 
 
+// Assignment operators 
+c = 30;
+d = c;
+f = 4;
+console.log(d);
+console.log(c+=f);
+console.log(c-=f);
+console.log(c*=f);
+console.log(c/=f);
+console.log(c+=f);
+console.log(c%=f);
+
+
+// Comparison operators 
+let age = 22;
+console.log(age > 18);
+console.log(age >= 22);
+console.log(age < 23);
+console.log(age <= 22);
+
+
+
+
+
+l
 
 
 

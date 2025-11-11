40d736446872ad2a0f0b5c602a245779ecd18bf5
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-11-11 20:55:20 +0500

    operators

---
+++ Operators/script.js
@@ -0,0 +1,20 @@
+// Arithmetic operators
+let a = 25;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(a ** b);
+console.log(a % b);
+
+// Unary operators
+console.log(a++);
+console.log(++a);
+console.log(b--);
+console.log(--b);
+
+
+
+
+

d045f8a833df35d4305413898f5475d2a18464f9
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-11-13 20:14:02 +0500

    Conditional Statements in js

--- Conditional_statements/script.js
+++ Conditional_statements/script.js
@@ -10,3 +10,20 @@ if (firstName == "Mumtaz") {
     console.log(`Welcome ${firstName}`);
     
 }
+
+
+// traffic lights
+
+let color = "green";
+if (color === "red") {
+    console.log("Stop!");
+    
+}
+if (color === "yellow") {
+    console.log("Get Ready");
+    
+}
+if (color === "green") {
+    console.log("Go");
+    
+}

0149cc2c44c5045ad07cf0605ece2c9b31878d57
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-11-25 22:13:15 +0500

    operators in js

--- Operators/script.js
+++ Operators/script.js
@@ -14,89 +14,75 @@ console.log(++a);
 console.log(b--);
 console.log(--b);
 
-
-// Assignment operators 
+// Assignment operators
 c = 30;
 d = c;
 f = 4;
 console.log(d);
-console.log(c+=f);
-console.log(c-=f);
-console.log(c*=f);
-console.log(c/=f);
-console.log(c+=f);
-console.log(c%=f);
+console.log((c += f));
+console.log((c -= f));
+console.log((c *= f));
+console.log((c /= f));
+console.log((c += f));
+console.log((c %= f));
 
-
-// Comparison operators 
+// Comparison operators
 let age = 22;
 console.log(age > 18);
 console.log(age >= 22);
 console.log(age < 23);
 console.log(age <= 22);
 
-
 // equal to  == not strickly check
 console.log(5 == 5);
-console.log(5 == '5'); //true
+console.log(5 == "5"); //true
 
 // === strickly check
-console.log(5 === '5'); //false
+console.log(5 === "5"); //false
 
 // not equal to !=
 console.log(5 != 5); //false
-console.log(5 != 4); //true 
-
-
+console.log(5 != 4); //true
 
 let markss = 93;
 if (markss >= 50 && markss >= 80) {
-    console.log("pass");
-    console.log("A+")
+  console.log("pass");
+  console.log("A+");
 }
 let marks = 52;
 if (marks >= 50 || marks >= 80) {
-    console.log("pass");
-    console.log("A+")
+  console.log("pass");
+  console.log("A+");
 }
 let Marks = 52;
 if (!(Marks < 33)) {
-    console.log("pass");
-    console.log("A+")
+  console.log("pass");
+  console.log("A+");
 }
 
 let Markss = 75;
-if ((Markss > 33 && Marks <= 80) || !false) {   // here it chack from left to right
-    console.log("pass");
-    
+if ((Markss > 33 && Marks <= 80) || !false) {
+  // here it chack from left to right
+  console.log("pass");
 }
 
 //!false or !true -> we avoid the values
 
-
 //Practice Q
-//Q- A "Good String" is a string that starts with letter 'a' and has a length > 3. 
+//Q- A "Good String" is a string that starts with letter 'a' and has a length > 3.
 //write a program to find if a string is good or not
 
 let str = "apple";
-if ((str[0] === 'a') && str.length > 3) {
-    console.log("Good String");
-    
-}else {
-    console.log("Not a good string");
-    
+if (str[0] === "a" && str.length > 3) {
+  console.log("Good String");
+} else {
+  console.log("Not a good string");
 }
-
 
 //Q-2
 let num = 12;
-if ((num % 3 === 0) && ((num + 1 == 15) || (num - 1 == 11))) {
-    console.log("Safe");
-    
-}else {
-    console.log("UnSafe");
-    
+if (num % 3 === 0 && (num + 1 == 15 || num - 1 == 11)) {
+  console.log("Safe");
+} else {
+  console.log("UnSafe");
 }
-
-
-

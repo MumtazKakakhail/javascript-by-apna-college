047d9e9cb5a4e69d353863a5bd1b706f8376d444
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-11-26 20:14:59 +0500

    switch statement in js

--- Conditional_statements/script.js
+++ Conditional_statements/script.js
@@ -165,35 +165,35 @@ switch (colour) {
 
 //use switch ststement to print the day of week using a number variable 'day' with value '1 to 7'
 let day = 1;
-switch(day){
+switch (day) {
     case "1":
-    console.log("monday")
-    break;
+        console.log("monday")
+        break;
     case "2":
         console.log("tuesday");
         break;
-        case "3":
-            console.log("wednesday");
-            break;
-            case "4":
-                console.log("thrusday");
-                break;
-                case "5":
-                    console.log("friday");
-                    break;
-                    case "6":
-                        console.log("saturday");
-                        break;
-                        case "7":
-                            console.log("sunday");
-                            break;
-                            default:
-                                console.log("Please Enter Number between 1 to 7");
-                                
-                            
-                        
-                    
-                
-            
-        
+    case "3":
+        console.log("wednesday");
+        break;
+    case "4":
+        console.log("thrusday");
+        break;
+    case "5":
+        console.log("friday");
+        break;
+    case "6":
+        console.log("saturday");
+        break;
+    case "7":
+        console.log("sunday");
+        break;
+    default:
+        console.log("Please Enter Number between 1 to 7");
+
+
+
+
+
+
+
 }

8d16f803f8e48d128d8d9547105c2db1f69a49a5
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-11-27 21:21:47 +0500

    Strings in Js

--- Strings/script.js
+++ Strings/script.js
@@ -1,3 +1,6 @@
+//string methods
+
+//trim
 let password = prompt("Enter Your Password");
 let newPass = password.trim();
 console.log(newPass);
@@ -6,3 +9,13 @@ console.log(newPass);
 
 //In js strings are immuteable means no changes can be made to string
 //whenever we do try to make a change , a new string is created and old one remains same.
+
+
+//tiuppercase and to uppercase
+
+let names = "mumtaz";
+console.log(names.toUpperCase());
+
+
+let lastNam = "WALI";
+console.log(lastNam.toLowerCase());

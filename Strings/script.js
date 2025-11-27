790bcef0163e5e947178fdeed48a892480c1cf1b
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-11-27 20:38:25 +0500

    Strings in Js

--- Strings/script.js
+++ Strings/script.js
@@ -1,3 +1,8 @@
 let password = prompt("Enter Your Password");
 let newPass = password.trim();
 console.log(newPass);
+
+
+
+//In js strings are immuteable means no changes can be made to string
+//whenever we do try to make a change , a new string is created and old one remains same.

83837e7f93ecc21c7111ba9ded73ad5e619ecf50
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-11-28 21:41:39 +0500

    Array and array methods in js

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,1 +1,8 @@
 let students = ["mumtaz","kamran","sameer"];
+console.log(students);
+
+//Arrays are are muteable in js
+console.log(students[1] = "shah");
+
+console.log(students[10] = "shaukat");
+

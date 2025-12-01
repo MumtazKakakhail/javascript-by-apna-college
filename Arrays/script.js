c85a07eb30f3cb0149118c2d31bd35fbe47795e5
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-12-01 19:20:23 +0500

    Array and array methods in js

--- Arrays/script.js
+++ Arrays/script.js
@@ -4,7 +4,15 @@ console.log(students);
 //Arrays are are muteable in js
 console.log((students[1] = "shah"));
 
-console.log((students[10] = "shaukat"));
+// console.log((students[10] = "shaukat"));
 
 
+//Array property
+//length
+console.log(students.length);
 
+//Array Methods
+//1- push
+console.log(students.push("saleem"));
+//2- pop
+console.log(students.pop());

1f371ee17f8ee26fd8f9fd1c84a9a7f457f301af
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-12-01 19:58:34 +0500

    Array and array methods in js

--- Arrays/script.js
+++ Arrays/script.js
@@ -21,3 +21,21 @@ console.log(students.unshift("shahab"));
 //4- shift
 console.log(students.shift());
 
+let followers = ["a", "b", "c"]
+console.log(followers);
+
+let blocked = followers.shift();
+
+
+//practice-Q
+//Q- for the given start state of the array,change it to final form using methods
+// start = ["january", "july", "march", "august"]
+// final = [ "july", "june" "march", "august"]
+let months = ["january", "july", "march", "august"]
+console.log(months.shift());
+console.log(months.shift());
+console.log(months.unshift("june"));
+console.log(months.unshift("july"));
+
+
+

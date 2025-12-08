4c7a3375d43ec083b387375839200acbeaf40b9f
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-12-08 22:01:14 +0500

    Loops in js

--- Loops/script.js
+++ Loops/script.js
@@ -44,3 +44,13 @@ for(let i=n; i<=n*10; i=i+n) {
     console.log(i);
     
 }
+
+//nested loop
+for(let i=1; i<=3; i++){
+    console.log(`outer loop ${i}`);
+    for(j=1; j<=3; j++) {
+        console.log(j);
+        
+    }
+    
+}

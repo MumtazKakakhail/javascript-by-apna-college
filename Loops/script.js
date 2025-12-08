04448343342ece33c6e1118a50c55fab92e98736
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-12-08 21:53:20 +0500

    Loops in js

--- Loops/script.js
+++ Loops/script.js
@@ -16,6 +16,31 @@ for (let i = 5; i >= 1; i--) {
 
 //print odd numbers
 for(let i=1; i<=15; i=i+2) {
+    console.log("odd numbers:",i);
+    
+}
+
+//print even numbers
+for(let i=2; i<=10; i=i+2) {
+    console.log("even numbers:",i);
+    
+}
+
+//backward
+for(let i=10; i>=2; i=i-2 ) {
+    console.log("backward:",i);
+    
+}
+
+//print multiplication table for 5
+for(let i=5; i<=50; i=i+5) {
+    console.log(i);
+    
+}
+
+let n = prompt("write your number");
+n = parseInt(n);
+for(let i=n; i<=n*10; i=i+n) {
     console.log(i);
     
 }

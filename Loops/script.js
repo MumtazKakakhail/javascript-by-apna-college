8b3aac1200547841d722b05ed7505f422ab2f0dc
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-12-12 21:19:22 +0500

    Practicing loops with arrays

--- Loops/script.js
+++ Loops/script.js
@@ -78,11 +78,19 @@ if (guess == favMovie) {
 }
 
 //break
-let m=1;
-while(m<=5) {
-    if (m == 3) {
-        break;
-    }
-    console.log(m);
+// let m=1;
+// while(m<=5) {
+//     if (m == 3) {
+//         break;
+//     }
+//     console.log(m);
+    
+// }
+
+//Loops with arrays
+let fruits = ["apple","banana","orange","mango","graps"];
+fruits.push("pineapple");
+for(let i=0; i<fruits.length; i++) {
+    console.log(i, fruits[i]);
     
 }

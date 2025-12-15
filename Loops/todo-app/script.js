5aa31a35f3d37fa0f4107f4ca3d1a46f4964b12f
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-12-15 23:08:46 +0500

    todo app using js

--- Loops/todo-app/script.js
+++ Loops/todo-app/script.js
@@ -13,8 +13,8 @@ while(true) {
     if(req == "list") {
         console.log("------------");
         
-        for(task of todo ){
-            console.log(task);
+        for(let i=0; i<todo.length; i++){
+            console.log(i, todo[i]);
             
         }
         console.log("------------");
@@ -23,5 +23,14 @@ while(true) {
         todo.push(task);
         console.log("task added");
         
+    }else if(req == "delete"){
+       let idx = prompt("please enter the task index");
+       todo.splice(idx, 1); 
+       console.log("task deleted");
+       
+    }else {
+        console.log("wrong request");
+        
     }
+    req = prompt("please enter your request");
 }

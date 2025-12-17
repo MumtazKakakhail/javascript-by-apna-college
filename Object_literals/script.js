dee4a5d90ed2d6dd209e232cdc6e77c24c9cd577
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-12-17 19:43:50 +0500

    object literals in js

--- Object_literals/script.js
+++ Object_literals/script.js
@@ -1,6 +1,28 @@
+const std = {
+name: "Ali",
+age: 21,
+marks: 97.2
+}
+console.log(std);
+
+
+//twitter post
+const post = {
+    username: "@mumtazkakakhail",
+    content: "This is my @First post",
+    likes: 320,
+    reposts: 3,
+    tags: ["@kamranahmad", "@shahabuddin"]
+};
+
+//get values
+post["username"];
+
+post.likes;
+
+
 const student = {
 name: "Ali",
 age: 21,
 marks: 97.2
 }
-console.log(student);

ea3f146d9fe8ce1856222f21f35cfe3dbfa8621b
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-12-17 21:26:57 +0500

    guessing game using js

---
+++ Object_literals/Guessing-game/script.js
@@ -0,0 +1,18 @@
+const max = prompt("enter the max number");
+const random = Math.floor(Math.random()*max);
+
+let guess = prompt("guess the number");
+
+while(true) {
+    if(guess == "quit") {
+ console.log("user quit");
+    break;
+
+    }if(guess == random) {
+    console.log("You are right! Congrats!!");
+    break;
+    
+}else {
+    prompt("your guess was wrong, please try again")
+}
+}

6a0710321ccdc596a94db9911c03f66479608389
mumtazkakakhail <mumtazkakakhail@gmail.com>
2026-05-01 22:32:45 +0500

    Functions with Arguments

--- Functions/script.js
+++ Functions/script.js
@@ -1,44 +1,44 @@
 //functions
 
-function hello(){
+function hello() {
     console.log("hello world!");
-    
+
 }
 hello();
 
-function adult(){
+function adult() {
     let age = 28;
-    if (age >= 18){
+    if (age >= 18) {
         console.log("Adult");
-        
-    }else {
+
+    } else {
         console.log("Under Age");
-        
+
     }
 }
 adult();
 
 
 //Functions with Arguments
-function printName(name){
-console.log(name);
+function printName(name) {
+    console.log(name);
 
 }
 printName("Mumtaz");
 
-function printInfo(name, age){
+function printInfo(name, age) {
     console.log(`${name}'s age is ${age}`);
-    
+
 }
 printInfo("maham", 9);
 printInfo("zenish", 10);
 printInfo("ajmal");
 
 
-function sum(a,b){
-    console.log(a+b);
-    
+function sum(a, b) {
+    console.log(a + b);
+
 }
-sum(1,2);
-sum(11,13);
-sum(13,26);
+sum(1, 2);
+sum(11, 13);
+sum(13, 26);

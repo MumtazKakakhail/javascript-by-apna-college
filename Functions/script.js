f5d395047b9e37c04ad337177aea609ee6fc7c19
mumtazkakakhail <mumtazkakakhail@gmail.com>
2026-05-01 10:34:33 +0500

    functions in js

---
+++ Functions/script.js
@@ -0,0 +1,20 @@
+//functions
+
+function hello(){
+    console.log("hello world!");
+    
+}
+hello();
+
+function adult(){
+    let age = 28;
+    if (age >= 18){
+        console.log("Adult");
+        
+    }else {
+        console.log("Under Age");
+        
+    }
+}
+adult();
+
